d04984f7234c63a650fb9829d1c3820d68bcbdfa
QUARTZRAINNIE\Duong <dqvu@umail.iu.edu>
2018-01-21 14:09:40 -0800

    Finish first version

--- src/BCproptax/app.R
+++ src/BCproptax/app.R
@@ -1,27 +1,17 @@
 #
 # This is a Shiny web application
-# An Exploration of South American countries
+# An Exploration of British Columbia Property Tax Transfer
 #
 
 library(shiny)
 library(tidyverse)
+library(tibble)
+library(glue)
 city_tax <- read_csv("../muni_tax.csv")
 
-tweaks <- 
-  list(tags$head(tags$style(HTML("
-                                 .multicol { 
-                                 height: 150px;
-                                 -webkit-column-count: 5; /* Chrome, Safari, Opera */ 
-                                 -moz-column-count: 5;    /* Firefox */ 
-                                 column-count: 5; 
-                                 -moz-column-fill: auto;
-                                 -column-fill: auto;
-                                 } 
-                                 "))
-  ))
 
 # Define UI for application that draws a map and two scatterplot
-ui <- fluidPage(tweaks,
+ui <- fluidPage(
   
   titlePanel("British Columbia Property Tax Transfer"),
   
@@ -37,13 +27,15 @@ ui <- fluidPage(tweaks,
                                "Vancouver", "Whistler"),
                      selected = c("Nanaimo", "Vancouver","Burnaby"), multiple = TRUE),
       
-      checkboxGroupInput("check", h3("Feature Selection"),
-                         choices = unique(city_tax$Statistics),
-                         selected = c("COMMERCIAL TOTAL (count)",
-                                         "RESIDENTIAL TOTAL (count)")),
-      br(),
-      br(),
-      br(),
+      selectInput("transType", h3("Transaction Type"),
+                  choices = c(Transaction_Number = "Count",
+                              Transaction_Value = "Dollar")),
+      
+      helpText(h5("Ctrl Click to select multiple features")),
+      uiOutput("whatever"),
+      
+
+    
       # Display 1st scatterplot input method
       helpText(h3("Adjust the two trend plots")),
       
@@ -56,32 +48,32 @@ ui <- fluidPage(tweaks,
                               "Vancouver", "Whistler"),
                   selected = "Nanaimo"),
       
-      sliderInput("slider", h3("Time range"),
-                  min = 1952, max = 2007, value = 1972, 
-                  step = 5, sep = "", animate = TRUE),
-      helpText("Adjust the trend line plot"),
+     
+      helpText(h3("Adjust the line plot")),
      
       selectInput("select2", h3("Feature Selection"),
-                  choices = c("COMMERCIAL - COMMERCE (count)",
-                              "COMMERCIAL - OTHER (count)"),
+                  choices = unique(city_tax$Statistics),
                   selected = "COMMERCIAL - COMMERCE (count)")
-      
+
     ),
     
     # Show three plots
     mainPanel(
-      h2("First barplot"),
+      h2("Overview"),
       plotOutput("barPlot"),
       
-      fluidRow(
-        column(6,
-        h2("First trend plot"),
-        plotOutput("trendplot1")),
-        column(6,
-               h2("Transaction amount of City A"),
-               plotOutput("value"))),
+      h2("Line trend plot"),
+        plotOutput("trendplot1"),
+      
+      # fluidRow(
+      #   column(6,
+      #   h2("First trend plot"),
+      #   plotOutput("trendplot1")),
+      #   column(6,
+      #          h2("Transaction amount of"),
+      #          textOutput("value"))),
         
-      h2("Second trend plot"),
+      h2("Stacked area plot"),
       plotOutput("trendplot2")
     )
   ))
@@ -89,6 +81,40 @@ ui <- fluidPage(tweaks,
 
 # Define server logic required 
 server <- function(input, output) {
+  
+  output$whatever <- renderUI({
+    count_dollar <- as.tibble(t(as.data.frame(list(
+      c("Count","COMMERCIAL - COMMERCE (count)"),
+      c("Count","COMMERCIAL - OTHER (count)"),
+      c("Count","COMMERCIAL - STRATA NON-RESIDENTIAL (count)"),
+      c("Count","COMMERCIAL TOTAL (count)"),
+      c("Count","FARM TOTAL (count)"),
+      c("Count","OTHER/UNKNOWN TOTAL (count)"),
+      c("Count","RECREATIONAL TOTAL (count)"),
+      c("Count","RESIDENTIAL - ACREAGE (count)"),
+      c("Count","RESIDENTIAL - COMMERCE (count)"),
+      c("Count","RESIDENTIAL - FARM (count)"),
+      c("Count","RESIDENTIAL - MULTI-FAMILY (count)"),
+      c("Count","RESIDENTIAL - OTHER (count)"),
+      c("Count","RESIDENTIAL - SINGLE FAMILY RESIDENTIAL (count)"),
+      c("Count","RESIDENTIAL - STRATA NON-RESIDENTIAL or RENTAL (count)"),
+      c("Count","RESIDENTIAL - STRATA RESIDENTIAL (count)"),
+      c("Count","RESIDENTIAL TOTAL (count)"),
+      c("Count","Total Market Transactions (count)"),
+      c("Count","Foreign Involvement Transactions (count)"),
+      c("Dollar","FMV Median of Foreign Involvement Transactions ($ median)"),
+      c("Dollar","FMV Sum of Foreign Involvement Transactions ($ sum)")))))
+
+    names(count_dollar) <- c('type', 'nameft')
+
+    selectInput("check", h3("Feature Type"),
+                choices=filter(count_dollar,
+                                 type==input$transType)$nameft,
+                selected = c("COMMERCIAL TOTAL (count)",
+                             "RESIDENTIAL TOTAL (count)",
+                             "Foreign Involvement Transactions (count)"),
+                multiple = TRUE, selectize = FALSE)
+  })
   
   output$barPlot <- renderPlot({
     
@@ -100,6 +126,20 @@ server <- function(input, output) {
     
   
   })
+  
+
+  # output$value <- renderText({ 
+  #   h3(glue("Total transaction number of {input$select} city"))
+  #   
+  #   x <- sum((city_tax %>% filter(Municipality == input$select,
+  #                                 !Statistics %in% 
+  #                                   c("FMV Median of Foreign Involvement Transactions ($ median)",
+  #                                     "FMV Sum of Foreign Involvement Transactions ($ sum)")))$values, na.rm = TRUE)
+  #   
+  #   h2(glue("{x} transactions"))
+  #   
+  # })
+  
   
   output$trendplot1 <- renderPlot({
     city_tax %>% 
@@ -119,18 +159,12 @@ server <- function(input, output) {
                              "RESIDENTIAL - FARM (count)",
                              "RESIDENTIAL - MULTI-FAMILY (count)")) %>% 
     ggplot(aes(x=date,y=values,group=Statistics,fill=Statistics)) + 
-    geom_area(position="fill", alpha = 0.8)+
-    scale_fill_manual(values=c("#7CB140", "#FFC000", "#ED7D31")) +
+    geom_area(position="fill")+
+    scale_fill_manual(values=c("#7CB140", "#FF2A7F","#FFAA00")) +
     theme(legend.position="bottom")
   })
   
-  output$value <- renderPrint({ 
-    h3(input$select)
-    x <- 100
-    h1(x)
-    h3("Average number of transaction")
-     })
-  
+
 }
 
 # Run the application 
